af5f16f1d8668a48beb75216c9a65e5b8d030a7f
Bogdan Tanase <bftanase@gmail.com>
2012-05-04 22:38:58 +0300

    tme site first commit

---
+++ addons/shared_addons/themes/cafe/theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Cafe extends Theme {
+
+    public $name = 'Cafe';
+    public $author = 'Free CSS Templates';
+    public $author_website = 'http://freecsstemplates.org';
+    public $website = '';
+    public $description = '2-column, fixed; horizontal nav.';
+    public $version = '1.0';
+
+}
+
+/* End of file theme.php */
